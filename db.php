5e9b1c00d7483c13c4dac6167daf224acec29424
Milos Tijanic <mikigrobar1997@gmail.com>
2021-03-23 20:31:03 +0100

    db.connection

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+    // ako su mysql username/password i ime baze na vasim racunarima drugaciji
+    // obavezno ih ovde zamenite
+    $servername = "127.0.0.1";
+    $username = "milos";
+    $password = "milos";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
